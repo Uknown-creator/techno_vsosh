5971c85458f97930a05dbec2e1006e1fa2f90fec
Uknown-creator <kuskov.work@gmail.com>
2024-01-25 21:50:13 +0300

    finally fix teachers

--- handlers/admin_func.py
+++ handlers/admin_func.py
@@ -57,7 +57,7 @@ async def add_teacher(
                 "Ошибка: неверно переданы аргументы. Пример:\n/add_teacher {ID человека}"
             )
             return
-        await users.add_teacher(user_id)
+        users.add_teacher(user_id)
         await message.answer("Готово")
 
 
